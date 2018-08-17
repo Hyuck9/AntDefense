7ec0894ca07c03b129796a91f7f36aaa8a83fc6f
Hyuck <lhg1304@gmail.com>
2018-08-17 20:45:51 +0900

    MainActivity ( 메인화면 ) 구성

--- app/src/main/java/me/hyuck/antdefense/MainActivity.kt
+++ app/src/main/java/me/hyuck/antdefense/MainActivity.kt
@@ -2,11 +2,32 @@ package me.hyuck.antdefense
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun mOnClick(view: View) {
+        when ( view.id ) {
+            R.id.main_btn_start -> {
+                // TODO : 스테이지 선택 창으로 이동
+            }
+            R.id.main_btn_exit -> finish()
+            R.id.main_btn_credit -> {
+                if ( main_img_credit.visibility == View.VISIBLE ) {
+                    main_img_credit.visibility = View.GONE
+                } else {
+                    main_img_credit.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
